c3eadbb30d8a397ff3d99721e9998bfd5ea23849
Luan Andrade <luan10017@hotmail.com>
2020-04-28 20:02:58 -0300

    Primeiro commit

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id:'GykTLqODQuU',
+        title:'Formuláario Animado com JS puro e CSS Animation',
+        duration:'57 min',
+        price:'Free'
+    },
+    {
+        id:'HN1UjzRSdBk',
+        title:'Desvendando o CSS grid na prática',
+        duration:'36 min',
+        price:'Free'
+    },
+    {
+        id:'2alg7MQ6_sI',
+        title:'Git e Github na prática',
+        duration:'33 min',
+        price:'Free'
+    },
+    {
+        id:'GSqR2i-Pq6o',
+        title:'Desvendando a variável this',
+        duration:'48 min',
+        price:'Free'
+    },
+    {
+        id:'rAzHvYnQ8DY',
+        title:'Array: high order functions',
+        duration:'54 min',
+        price:'Free'
+    },
+    {
+        id:'ghTrp1x_1As',
+        title:'O que é API? Rest e Restful',
+        duration:'33 min',
+        price:'Free'
+    },
+]
