cce35dd43f8588d79f93565f95fdd24073013838
Luan Andrade <luan10017@hotmail.com>
2020-05-02 21:05:19 -0300

    Adicionada página de erro 404

--- server.js
+++ server.js
@@ -6,50 +6,52 @@ const videos = require("./data")
 
 server.use(express.static('public'))
 
-server.set('view engine','njk')
+server.set('view engine', 'njk')
 
-nunjucks.configure('views',{
+nunjucks.configure('views', {
     express: server,
     autoescape: false,
     noCache: true
 })
 
-server.get('/', function(req, res) {
+server.get('/', function (req, res) {
     const about = {
-       avatar_url: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-       name: 'Mayk Brito',
-       role: 'Instrutor - Rocketseat',
-       description: 'Programador full stack, focado em trazero o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br/" target="_blanck">Rocketseat</a>',
-       links:[
-           {name: 'Github', url: 'https://github.com/maykbrito'},
-           {name: 'Linkedin', url: 'https://www.linkedin.com/in/maykbrito/?originalSubdomain=b'},
-       ]
+        avatar_url: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        name: 'Mayk Brito',
+        role: 'Instrutor - Rocketseat',
+        description: 'Programador full stack, focado em trazero o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br/" target="_blanck">Rocketseat</a>',
+        links: [
+            { name: 'Github', url: 'https://github.com/maykbrito' },
+            { name: 'Linkedin', url: 'https://www.linkedin.com/in/maykbrito/?originalSubdomain=b' },
+        ]
     }
 
-    return res.render('about',{ about })
+    return res.render('about', { about })
 })
 
-server.get('/portfolio', function(req, res) {
+server.get('/portfolio', function (req, res) {
 
-    return res.render('portfolio',{items: videos})
+    return res.render('portfolio', { items: videos })
 })
 
-server.get('/video', function(req, res){
+server.get('/video', function (req, res) {
     const id = req.query.id
 
-    const video = videos.find(function(video){
+    const video = videos.find(function (video) {
         return video.id == id
     })
-    
+
     if (!video) {
         return res.send('Video not found!')
     }
 
-    return res. render('video', {item: video})
-
+    return res.render('video', { item: video })
 })
 
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+});
 
-server.listen(5000, function(){
+server.listen(5000, function () {
     console.log('Server is running')
 })
